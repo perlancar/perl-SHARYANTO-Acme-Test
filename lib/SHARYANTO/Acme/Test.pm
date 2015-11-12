2ea06c6bad6b06c2574347e50b63e5d7b3102c99
Steven Haryanto (@netbook-zenbook) <stevenharyanto@gmail.com>
2015-11-12 15:29:19 +0700

    Initial commit

---
+++ lib/SHARYANTO/Acme/Test.pm
@@ -0,0 +1,18 @@
+package SHARYANTO::Acme::Test;
+
+use 5.010001;
+use strict;
+use warnings;
+
+1;
+__END__
+# ABSTRACT: Distribution to test various things
+
+# DATE
+# DIST
+# VERSION
+
+=head1 DESCRIPTION
+
+You should not be using this module. This distribution contains various tests
+that are mostly only of concern to me.
